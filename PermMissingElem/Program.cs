fb843b61abdf01022cf7f4e86d6b8984d4dcc15b
LaszloOvari <laszlo.ovari@ymail.com>
2020-01-08 14:47:32 +0100

    Add project files.

---
+++ PermMissingElem/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PermMissingElem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = { 1, 2, 4 };
+            Console.WriteLine(Solution(A));
+        }
+
+        public static int Solution(int[] A)
+        {
+            return (int)((A.Length == 0) ? 1 : ((long)(A.Length + 1) * (A.Length + 2) / 2) - A.Sum(x => (long)x));
+        }
+    }
+}
